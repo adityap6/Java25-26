00f62203b8d4498f478f12d8e53f9f8bfe001b05
adityap6 <adityap6@nycstudents.net>
2025-09-16 16:28:29 +0000

    nkjngskjdfng

---
+++ Java-4-Discussion/Main.java
@@ -0,0 +1,26 @@
+class Main {
+  
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function runs when the program begins.  
+
+  void init(){
+    // Number of cars
+       int cars = 20; 
+    // Investment amount in currency
+      float amount = 156.39f;
+    // Person's first name
+      String name = "Aditya";
+    // Is a game over?
+      boolean isGameOver = false; 
+    // Store the first initial of a name
+      char finitial ='a';
+    // Maximum number of customers in a theater (declare as a constant)
+      final int MaxCust = 500;
+
+  }
+
+  
+}
