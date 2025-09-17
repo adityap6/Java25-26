5c002b31f55f7c89746caa858b1692c2f9b73b2a
adityap6 <adityap6@nycstudents.net>
2025-09-17 16:16:08 +0000

    seventeenth

---
+++ Java-4-Activity/Main.java
@@ -0,0 +1,55 @@
+class Main {
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+ void init(){
+// Define a variable for each situation in the challenges below. Assign a value to each variable.
+// Display the value of each variable to the console.
+
+/*  Challenge 1:    
+    1) student's age 
+    2) number of people that live with you
+    3) number of items in a cart 
+*/
+   int age = 24; 
+   int people = 32;
+   int items = 12; 
+   System.out.println(age);
+   System.out.println(people);
+   System.out.println(items);
+/*  Challenge 2:
+    1) temperature 
+    2) student's GPA
+    3) growth rate of your investment
+*/
+    float temp = 32.5f; 
+    float grade = 4.0f; 
+    float rate = 3.5f; 
+
+
+/*  Challenge 3:
+    1) Student's last name 
+    2) student's first initial
+    3) description of an item 
+*/
+    String lname = "Smith"; 
+    char finitial = 'a';
+    String desc = "20 lbs, gray, useful";  
+
+/*  Challenge 4:
+    1) the status whether lights are on
+    2) the status whether the game is over
+    3) the status whether the battery is charging
+*/
+    boolean lightsOn = true; 
+    boolean gameOver = false; 
+    boolean batteryCharge = true;
+
+
+
+    // **************************************************
+    // **** Don't write any code below here.  ***********
+    // **************************************************
+  }
+}
