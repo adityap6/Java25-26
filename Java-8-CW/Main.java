c2024c3825b903d2d7002a9ee87b96eaeb68245e
adityap6 <adityap6@nycstudents.net>
2025-10-01 16:33:47 +0000

    gnsdkjgnjksdfngsd

---
+++ Java-8-CW/Main.java
@@ -0,0 +1,22 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  // define the functions
+string madlib("noun", "adjective", "verb"); 
+  string texts = ""
+  
+  
+double areaSquare(double side){
+    double area = side * side; 
+    return area;
+}
+  void init(){
+    // call the functions
+    double unit = areaSquare(3);
+    System.out.println(unit);
+  }
+
+ 
+  
+}
